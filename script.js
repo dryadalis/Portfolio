d0a116ee9ae4e8d085cb00b17f2d1a8ddfac1fff
Karolina <kika.romanska@gmail.com>
2018-02-19 18:58:02 +0100

    Navigator added

---
+++ script.js
@@ -0,0 +1,16 @@
+const navbar = document.getElementById('navbar');
+const topOfNav = navbar.offsetTop;
+
+console.log(topOfNav);
+
+function fixedNavbar() {
+    if(window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = navbar.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+
+}
+window.addEventListener('scroll', fixedNavbar);
